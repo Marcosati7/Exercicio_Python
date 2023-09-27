c573f7204c1230609cbcc48fc40c6f608b5aef01
Marcosati7 <136663316+Marcosati7@users.noreply.github.com>
2023-09-27 20:54:48 -0300

    Add files via upload

---
+++ Area do quadrado e seu dobro.py
@@ -0,0 +1,8 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+entrada=int(input('Insira o lado do quadrada: \n'))
+area=entrada**2
+dobro=area*2
+
+print('O valor da area do quadrado eh: ',area)
+print('O dobro do valor eh: ',dobro)
