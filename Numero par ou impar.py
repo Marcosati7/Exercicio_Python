3a5f21829dbf0694842ae81923734ae8b4d9461f
Marcosati7 <136663316+Marcosati7@users.noreply.github.com>
2023-09-27 21:14:05 -0300

    Add files via upload

---
+++ Numero par ou impar.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+entrada=int(input('Insira o numero:\n'))
+
+if entrada%2==0:
+    print('O numero digitado eh par')
+else:
+    print('O numero digitado eh impar')
