04a2a9bd6d497c5a5327f13c941065213765ca7f
Marcosati7 <136663316+Marcosati7@users.noreply.github.com>
2023-09-27 21:10:03 -0300

    Add files via upload

---
+++ Estrutura de repeticao for.py
@@ -0,0 +1,17 @@
+#Estrutura de Repetição
+# atividade 5
+
+#Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+#Depois modifique o programa para que ele mostre os números um ao lado do outro.
+lista=[]
+for i in range(1,21,1):
+    print(i)
+    lista.append(i)
+
+print(lista)
+
+
+
+
+
+
